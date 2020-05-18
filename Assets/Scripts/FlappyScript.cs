cfc66364bbb8c8bb2a16a386cde21fcf66febc2e
HernandaCandraP <hernandacp@gmail.com>
2020-05-18 18:40:03 +0700

    add script Flappy and GameState

---
+++ Assets/Scripts/FlappyScript.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlappyScript : MonoBehaviour
+{
+
+    public AudioClip FlyAudioClip, DeathAudioClip, ScoredAudioClip;
+    public Sprite GetReadySprite;
+    public float RotateUpSpeed = 1, RotateDownSpeed = 1;
+    public GameObject IntroGUI, DeathGUI, Score, miniScore;
+    public Collider2D restartButtonGameCollider;
+    public float VelocityPerJump = 3;
+    public float XSpeed = 1;
+    public GameObject StoppedFloor;
+    public Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        Score.SetActive(false);
+        miniScore.SetActive(false);
+    }
+
+    FlappyYAxisTravelState flappyYAxisTravelState;
+
+    enum FlappyYAxisTravelState
+    {
+        GoingUp, GoingDown
+    }
+
+    Vector3 birdRotation = Vector3.zero;
+    // Update is called once per frame
+    void Update()
+    {
+        //untuk tampilan handphone
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+        if (GameStateManager.GameState == GameState.Intro)
+        {
+            MoveBirdOnXAxis();
+            if (WasTouchedOrClicked())
+            {
+                BoostOnYAxis();
+                GameStateManager.GameState = GameState.Playing;
+                IntroGUI.SetActive(false);
+                Score.SetActive(false);
+                miniScore.SetActive(true);
+                ScoreManagerScript.Score = 0;
+            }
+        }
+
+        else if (GameStateManager.GameState == GameState.Playing)
+        {
+            MoveBirdOnXAxis();
+            if (WasTouchedOrClicked())
+            {
+                BoostOnYAxis();
+            }
+
+        }
+
+        else if (GameStateManager.GameState == GameState.Dead)
+        {
+            Vector2 contactPoint = Vector2.zero;
+
+            if (Input.touchCount > 0)
+                contactPoint = Input.touches[0].position;
+            if (Input.GetMouseButtonDown(0))
+                contactPoint = Input.mousePosition;
+
+            //cek restart game
+            if (restartButtonGameCollider == Physics2D.OverlapPoint
+                (Camera.main.ScreenToWorldPoint(contactPoint)))
+            {
+                GameStateManager.GameState = GameState.Intro;
+                Application.LoadLevel(Application.loadedLevelName);
+            }
+        }
+
+    }
+
+
+    void FixedUpdate()
+    {
+        //fixed jump in screen
+        if (GameStateManager.GameState == GameState.Intro)
+        {
+            if (GetComponent<Rigidbody2D>().velocity.y < -1) //when the speed drops, give a boost
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, GetComponent<Rigidbody2D>().mass * 5500 * Time.deltaTime)); //lots of play and stop 
+        }
+        else if (GameStateManager.GameState == GameState.Playing || GameStateManager.GameState == GameState.Dead)
+        {
+            FixFlappyRotation();
+        }
+    }
+
+    bool WasTouchedOrClicked()
+    {
+        if (Input.GetButtonUp("Jump") || Input.GetMouseButtonDown(0) || 
+            (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended))
+            return true;
+        else
+            return false;
+    }
+
+    void MoveBirdOnXAxis()
+    {
+        transform.position += new Vector3(Time.deltaTime * XSpeed, 0, 0);
+    }
+
+    void BoostOnYAxis()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, VelocityPerJump);
+        GetComponent<AudioSource>().PlayOneShot(FlyAudioClip);
+    }
+
+    /// flappy goes up, it'll rotate up to 45 degrees. when it falls, rotation will be -90 degrees
+    private void FixFlappyRotation()
+    {
+        if (GetComponent<Rigidbody2D>().velocity.y > 0) flappyYAxisTravelState = FlappyYAxisTravelState.GoingUp;
+        else flappyYAxisTravelState = FlappyYAxisTravelState.GoingDown;
+
+        float degreesToAdd = 0;
+
+        switch (flappyYAxisTravelState)
+        {
+            case FlappyYAxisTravelState.GoingUp:
+                degreesToAdd = 6 * RotateUpSpeed;
+                break;
+            case FlappyYAxisTravelState.GoingDown:
+                degreesToAdd = -3 * RotateDownSpeed;
+                break;
+            default:
+                break;
+        }
+
+        //-90<rotation<45 *always*
+        birdRotation = new Vector3(0, 0, Mathf.Clamp(birdRotation.z + degreesToAdd, -90, 45));
+        transform.eulerAngles = birdRotation;
+    }
+
+    /// check for collision with pipes
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (GameStateManager.GameState == GameState.Playing)
+        {
+            if (col.gameObject.tag == "Pipeblank") //pipeblank is an empty 
+            {
+                GetComponent<AudioSource>().PlayOneShot(ScoredAudioClip);
+                ScoreManagerScript.Score++;
+            }
+            else if (col.gameObject.tag == "Pipe")
+            {
+                FlappyDies();
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (GameStateManager.GameState == GameState.Playing)
+        {
+            if (col.gameObject.tag == "Floor")
+            {
+                FlappyDies();
+            }
+        }
+    }
+
+    void FlappyDies()
+    {
+        GameStateManager.GameState = GameState.Dead;
+        DeathGUI.SetActive(true);
+        Score.SetActive(true);
+        miniScore.SetActive(false);
+        GetComponent<AudioSource>().PlayOneShot(DeathAudioClip);
+        StoppedFloor.GetComponent<FloorMoveScript>().enabled = false;
+        anim.GetComponent<Animator>().Stop();
+    }
+
+}
